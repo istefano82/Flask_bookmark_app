a8b5c4b47b21c03b710137883060117906e8097b
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2018-12-05 22:43:29 +0200

    Basic templates and Views setup

--- Pluralsight_FLASK/thermos/thermos/thermos.py
+++ Pluralsight_FLASK/thermos/thermos/thermos.py
@@ -6,7 +6,25 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           title="Title passed from view to template",
+                           text='ala bala')
+
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
