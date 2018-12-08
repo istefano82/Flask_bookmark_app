3371a289ef076f8196523d876e5b4deae3c03f1d
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2018-12-08 23:09:31 +0200

    Add Flask manager script for db init and drop

--- Pluralsight_FLASK/thermos/thermos/thermos.py
+++ Pluralsight_FLASK/thermos/thermos/thermos.py
@@ -4,35 +4,24 @@ from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
-#from forms import BookmarkForm
+from forms import BookmarkForm
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = b'\xca\x9a\x0e\x18k\x8c\x88\x94\xfe\xbb\x06\xaf\x9e/\x10^`#n\x1d\xd0v\xe5\xbb'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
+app.config.setdefault(
+            'SQLALCHEMY_TRACK_MODIFICATIONS', True
+        )
 db = SQLAlchemy(app)
-
-bookmarks = []
-
-
-def store_bookmark(url, description):
-    bookmarks.append(dict(
-        url=url,
-        description=description,
-        user='istefano',
-        date=datetime.utcnow()
-    ))
-
-
-def new_bookmarks(num):
-    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
-
+import models
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', new_bookmarks=new_bookmarks(5), reverse=True)
+    return render_template('index.html', new_bookmarks=models.Bookmark.newest(5), reverse=True)
 
 
 @app.route('/add', methods=['GET', 'POST'])
@@ -41,7 +30,9 @@ def add():
     if form.validate_on_submit():
         url = form.url.data
         description = form.description.data
-        store_bookmark(url, description)
+        bm = models.Bookmark(url=url, description=description)
+        db.session.add(bm)
+        db.session.commit()
         flash("Stored bookmark '{}'".format(description))
         return redirect(url_for('index'))
     return render_template('add.html', form=form)
