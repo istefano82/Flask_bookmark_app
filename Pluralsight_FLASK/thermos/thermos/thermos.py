fe71c31636713e5892d29dd2bce4e549bd0ae532
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2018-12-08 10:39:14 +0200

    FOrms and View logic

--- Pluralsight_FLASK/thermos/thermos/thermos.py
+++ Pluralsight_FLASK/thermos/thermos/thermos.py
@@ -1,19 +1,44 @@
-from flask import Flask, render_template, url_for
+from datetime import datetime
+
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+from forms import BookmarkForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = b'\xca\x9a\x0e\x18k\x8c\x88\x94\xfe\xbb\x06\xaf\x9e/\x10^`#n\x1d\xd0v\xe5\xbb'
+
+bookmarks = []
+
+
+def store_bookmark(url, description):
+    bookmarks.append(dict(
+        url=url,
+        description=description,
+        user='istefano',
+        date=datetime.utcnow()
+    ))
+
+
+def new_bookmarks(num):
+    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html',
-                           title="Title passed from view to template",
-                           text='ala bala')
+    return render_template('index.html', new_bookmarks=new_bookmarks(5), reverse=True)
 
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add():
-    return render_template('add.html')
+    form = BookmarkForm()
+    if form.validate_on_submit():
+        url = form.url.data
+        description = form.description.data
+        store_bookmark(url, description)
+        flash("Stored bookmark '{}'".format(description))
+        return redirect(url_for('index'))
+    return render_template('add.html', form=form)
 
 
 @app.errorhandler(404)
