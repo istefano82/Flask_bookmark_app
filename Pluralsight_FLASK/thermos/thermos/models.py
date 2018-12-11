2660af0e7578d3bb6afb32f55ce31677516f2627
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2018-12-12 00:14:14 +0200

    Add many to many relation between bookmarks and tags

--- Pluralsight_FLASK/thermos/thermos/models.py
+++ Pluralsight_FLASK/thermos/thermos/models.py
@@ -7,12 +7,19 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from thermos import db
 
 
+tags = db.Table('bookmark_tag',
+                db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
+                db.Column('bookmark_id', db.Integer, db.ForeignKey('bookmark.id'))
+                )
+
 class Bookmark(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
     description = db.Column(db.String(300))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    _tags = db.relationship('Tag', secondary=tags,
+                            backref=db.backref('bookmarks', lazy='dynamic'))
 
     @staticmethod
     def newest(num):
@@ -46,3 +53,11 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return "<User '{}'>".format(self.username)
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False, unique=True, index=True)
+
+    def __repr__(self):
+        return self.name
