a196231fa755323ac51c667596b06210970b6655
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2018-12-11 23:43:03 +0200

    Add database migrations managing option

--- Pluralsight_FLASK/thermos/manage.py
+++ Pluralsight_FLASK/thermos/manage.py
@@ -1,10 +1,15 @@
 #! /usr/bin/env python
 
 from thermos import app, db
-from thermos.models import User
+from thermos.models import User, Bookmark
 from flask_script import Manager, prompt_bool
+from flask_migrate import Migrate, MigrateCommand
 
 manager = Manager(app)
+migrate = Migrate(app, db)
+
+manager.add_command('db', MigrateCommand)
+
 
 @manager.command
 def initdb():
@@ -14,12 +19,14 @@ def initdb():
     db.session.commit()
     print('Initialized the database')
 
+
 @manager.command
 def dropdb():
     if prompt_bool(
-        "Are you sure you want to lose all your data"):
+            "Are you sure you want to lose all your data"):
         db.drop_all()
         print('Dropped the database')
+
 
 if __name__ == '__main__':
     manager.run()
